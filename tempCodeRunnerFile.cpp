2c54b03394464205a595c7219ae26c417a915245
[LongJinhe-coder] <[909642437@qq.com]>
2019-12-30 11:10:03 +0800

    2019 12 30 update

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+cout<<"插入一个新结点"<<endl;
+    // int k;
+    // cin>>k;
+    // InsertTree(t,k);
+    // PrintTree(t);
+    // cout<<endl;
+    // cout<<"搜索结点"<<endl;
+    // int a;
+
+    // cin>>a;
+    // BiTree s;
+    
+    // cout<<"值=";
+    // cout<<SearchTree(t,s,a)->data;
